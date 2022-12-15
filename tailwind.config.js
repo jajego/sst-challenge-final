6c06eb013001c55138c6b4bb12acfea64dc231fe
Jamie <yames.git@gmail.com>
2022-12-14 20:55:09 -0500

    Removed test input list

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 
-const percentageWidth = require("tailwindcss-percentage-width");
 module.exports = {
   content: ["./src/**/*.{js,jsx}"],
   theme: {
     extend: {},
   },
-  plugins: [percentageWidth],
+  plugins: [],
 };
