7effcbb2a670ab57ebec0f922dbf0b6304fa8d76
Jamie <yames.git@gmail.com>
2022-12-17 16:26:53 -0500

    code spacing

--- src/components/Event/AddEventForm.js
+++ src/components/Event/AddEventForm.js
@@ -54,22 +54,22 @@ export default function AddEventForm({ events, setEvents, setFormOpen }) {
   };
 
   let inputStyle = `
-        form-control
-        block
-        w-42
-        px-3
-        py-1.5
-        text-base
-        font-normal
-        text-gray-700
-        bg-white bg-clip-padding
-        border border-solid border-gray-300
-        rounded
-        transition
-        ease-in-out
-        m-0
-        focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
-  `;
+                    form-control
+                    block
+                    w-42
+                    px-3
+                    py-1.5
+                    text-base
+                    font-normal
+                    text-gray-700
+                    bg-white bg-clip-padding
+                    border border-solid border-gray-300
+                    rounded
+                    transition
+                    ease-in-out
+                    m-0
+                    focus:text-gray-700 focus:bg-white focus:border-blue-600 focus:outline-none
+                  `;
 
   return (
     <div className="form-wrapper flex flex-col items-center w-50">
@@ -97,16 +97,16 @@ export default function AddEventForm({ events, setEvents, setFormOpen }) {
         />
         <button
           className="inline-block 
-        w-2/5
-        px-4 
-        py-2 
-        font-medium 
-        border
-        border-2
-        border-gray
-        rounded 
-        bg-white
-        focus:shadow-md focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg "
+                    w-2/5
+                    px-4 
+                    py-2 
+                    font-medium 
+                    border
+                    border-2
+                    border-gray
+                    rounded 
+                    bg-white
+                    focus:shadow-md focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg "
           type="submit"
         >
           Add
