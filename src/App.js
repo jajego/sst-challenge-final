5bafc11b57c0198c5f8704a2e8f2f040415efc25
Jamie <yames.git@gmail.com>
2022-12-17 14:55:08 -0500

    Added 2 more randomize buttons

--- src/App.js
+++ src/App.js
@@ -15,10 +15,10 @@ function App() {
     }
   }, [events]);
 
-  const randomize = () => {
+  const randomize = (howMany) => {
     setFormOpen(false);
 
-    return setEvents(generateInput(50));
+    return setEvents(generateInput(howMany));
   };
 
   const resetTimeline = () => {
@@ -31,6 +31,11 @@ function App() {
     setFormOpen(true);
   };
 
+  const buttonClass = `
+                  inline-block w-1/8 px-4 py-2 font-medium border border-2 border-gray rounded 
+                  bg-white focus:shadow-md focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg
+  `;
+
   return (
     <div className="timeline-wrapper w-full flex flex-col justify-center items-center gap-10 p-10">
       <div className="text-3xl font-bold text-white bg-gray-600 px-5 py-2 rounded-full">
@@ -38,24 +43,33 @@ function App() {
       </div>
       <div className="header-buttons flex gap-3 justify-center">
         <button
-          className="inline-block w-1/8 px-4 py-2 font-medium border border-2 border-gray rounded 
-                  bg-white focus:shadow-md focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg"
-          onClick={randomize}
+          className={`${buttonClass}`}
+          onClick={() => {
+            randomize(50);
+          }}
         >
-          Randomize
+          Randomize 50
         </button>
         <button
-          className="inline-block w-1/8 px-4 py-2 font-medium border border-2 border-gray rounded 
-                  bg-white focus:shadow-md focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg"
-          onClick={handleFormOpen}
+          className={`${buttonClass}`}
+          onClick={() => {
+            randomize(250);
+          }}
         >
+          Randomize 250
+        </button>
+        <button
+          className={`${buttonClass}`}
+          onClick={() => {
+            randomize(1000);
+          }}
+        >
+          Randomize 1000
+        </button>
+        <button className={`${buttonClass}`} onClick={handleFormOpen}>
           Add 1 event
         </button>
-        <button
-          className="inline-block w-1/8 px-4 py-2  font-medium border border-2 border-gray
-                  bg-white focus:shadow-md focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg"
-          onClick={resetTimeline}
-        >
+        <button className={`${buttonClass}`} onClick={resetTimeline}>
           Reset timeline
         </button>
       </div>
